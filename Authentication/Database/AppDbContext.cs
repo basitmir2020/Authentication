dab7571c31106b733b9e9e90015ca9b422c61bce
Basit Mir <42338600+basitmir2020@users.noreply.github.com>
2022-04-11 15:04:35 +0530

    Add project files.

---
+++ Authentication/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Authentication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Database
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions identityOptions) : base(identityOptions)
+        { }
+
+
+    }
+}
